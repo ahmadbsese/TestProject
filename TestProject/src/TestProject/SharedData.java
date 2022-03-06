87a43dec4dd4cdcb3ae1e7cb16d68fe4af7be635
MoayadHamza <itsmoayadbro@gmail.com>
2022-03-06 17:56:47 +0200

    third commit

--- TestProject/src/TestProject/SharedData.java
+++ TestProject/src/TestProject/SharedData.java
@@ -13,22 +13,35 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * @param array				Array with the numbers
+	 * @param b					The sum we are looking for
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * @return			return the winning array
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * @param winArray			Set the winning array
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return		return the numbers array
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
